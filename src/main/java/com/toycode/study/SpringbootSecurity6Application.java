45dda27f8d31df311b64c543212fc8b80799e856
hyeonseokhan <toycode727@gmail.com>
2023-03-20 14:55:16 +0900

    :tada: Initial commit

---
+++ src/main/java/com/toycode/study/SpringbootSecurity6Application.java
@@ -0,0 +1,13 @@
+package com.toycode.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurity6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurity6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/toycode/study/SpringbootSecurity6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toycode.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurity6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
